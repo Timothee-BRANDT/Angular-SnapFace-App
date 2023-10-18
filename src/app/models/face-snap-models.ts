0c2a2ec1b5c7f06df35c3f13e27b2b37eaf12c19
Tim <timothee@metacard.gift>
2023-10-18 09:40:09 +0200

    .

--- src/app/models/face-snap-models.ts
+++
@@ -1,9 +0,0 @@
-// public mean this class got all thoses attributes, and you can passed them to the constructor agruments
-export class FaceSnap {
-    constructor(public title: string,
-                public description: string,
-                public imageUrl: string,
-                public createdDate: Date,
-                public snaps: number) {
-    };
-} 
